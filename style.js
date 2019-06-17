53cb1039a4b252fb23cb9b8ff90d1c2be0dd11e7
Rasmus Vahtra <43164799+rasmvah@users.noreply.github.com>
2019-06-17 12:00:07 +0300

    Updated files.

--- style.js
+++ style.js
@@ -14,3 +14,147 @@ function myFunction() {
 function clearContents(element) {
   element.value = '';
 }
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown1").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown2").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown3").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown4").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown5").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown6").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown7").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown8").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown9").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown10").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown11").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown12").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown13").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown14").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown15").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown16").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown17").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown18").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown19").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = -100; i <= 100; i++){
+    $("#dropdown20").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = 0; i <= 480; i++){
+    $("#dropdown21").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = 0; i <= 480; i++){
+    $("#dropdown22").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = 0; i <= 480; i++){
+    $("#dropdown23").append("<option>"+i+"</option>");
+  }
+});
+
+$(document).ready(function(){
+  for(i = 0; i <= 480; i++){
+    $("#dropdown24").append("<option>"+i+"</option>");
+  }
+});
