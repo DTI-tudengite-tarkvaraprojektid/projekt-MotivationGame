f68d56cac53e74008c9ab99c3cf1b4c3b8e692ce
Urmot Rosenberg <43164656+urmotr@users.noreply.github.com>
2019-06-20 12:45:03 +0300

    fixes

--- js/gameselector.js
+++ js/gameselector.js
@@ -1,5 +1,10 @@
 /*jshint esversion: 6 */
 $(document).ready(function() {
+  document.getElementById('json-file').onchange = function() {
+    var txt = document.getElementById("json-file").value;
+    txt = txt.substring(12);
+    document.getElementById("file-name").innerHTML = txt;
+};
   document.getElementById("defaultstory").onclick = clearStorage;
 });
 function clearStorage(){
--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   if (localStorage.getItem("Story") === null) {
     document.getElementById("gameName").innerHTML = "";
   } else {
-    story = JSON.parse(localStorage.getItem('Story'));
+    var story = JSON.parse(localStorage.getItem('Story'));
+    console.log(story);
     document.getElementById("gameName").innerHTML = story.Task;
   }
 });
