92f7db6018df1b58c34ffc31c3c1f083381900b4
Urmot Rosenberg <43164656+urmotr@users.noreply.github.com>
2019-06-20 14:18:11 +0300

    uus nimi

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   if (localStorage.getItem("Story") === null) {
-    document.getElementById("gameName").innerHTML = "";
+    document.getElementById("gameName").innerHTML = "Complete two homework assignments in 3 days. The assignments need to be done well.";
   } else {
     var story = JSON.parse(localStorage.getItem('Story'));
     console.log(story);
